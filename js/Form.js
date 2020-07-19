db44ed54a0751267764c1973236b20ff564c703c
Abdulazeem <shaikhabdulazeem0@gmail.com>
2020-07-19 18:48:26 +0530

    CarGame2.5

--- js/Form.js
+++ js/Form.js
@@ -5,12 +5,14 @@ class Form {
     this.button = createButton('Play');
     this.greeting = createElement('h2');
     this.title = createElement('h2');
+    this.reset=createButton('Reset');
   }
   hide(){
     this.greeting.hide();
     this.button.hide();
     this.input.hide();
     this.title.hide();
+
   }
 
   display(){
@@ -20,6 +22,8 @@ class Form {
     this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
     this.button.position(displayWidth/2 + 30, displayHeight/2);
 2
+    this.reset.position(displayWidth - 100, 50 );    
+
     this.button.mousePressed(()=>{
       this.input.hide();
       this.button.hide();
@@ -30,7 +34,13 @@ class Form {
       player.updateCount(playerCount);
       this.greeting.html("Hello " + player.name)
       this.greeting.position(displayWidth/2 - 70, displayHeight/4);
+      
     });
+    this.reset.mousePressed(()=>{
+      player.updateCount(0);
+      game.update(0);
 
+
+    } );
   }
 }
